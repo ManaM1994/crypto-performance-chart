5d3a5351d67d456c304b5565c4b01fbde4ba58e8
ManaM1994 <mana94mobahi@gmail.com>
2025-05-14 12:22:13 +0330

    change layout to ltr

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="en" dir="ltr">
       <body className={`${inter.variable} antialiased bg-gray-50`}>
         <main className="container mx-auto px-4 py-8">{children}</main>
       </body>
